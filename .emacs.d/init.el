412a691ecd2a6baa30db9217a594738db3dd1d90
Robert Bartlensky <bartlensky.robert@gmail.com>
2023-02-06 00:57:59 +0000

    emacs: improve startup times

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,34 +1,33 @@
-;;; init.el --- starts here
-;;; Commentary:
-;;; Code:
-
-(package-initialize)
-
-(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+(require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
 
-(unless (package-installed-p 'use-package)
-    (package-refresh-contents)
-    (package-install 'use-package))
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
 
-(require 'use-package)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
 (eval-when-compile (require 'use-package))
+(require 'bind-key)
 
 ;; misc things
 (tool-bar-mode -1) ;; no more toolbar
+(menu-bar-mode -1) ;; no more menu bar
+(scroll-bar-mode -1) ;; no more scroll bar
 (global-linum-mode 1) ;; line numbers
 (delete-selection-mode) ;; when copying over a selected text, delete it
 (global-hl-line-mode 1) ;; highlight current line
 (show-paren-mode 1) ;; show matching parenthesis
 (electric-pair-mode 1) ;; closes brackets when opening one
-(windmove-default-keybindings) ;; shit-arrow to move around windows
-
-(setq column-number-mode t) ;; column numbers
+(windmove-default-keybindings) ;; shit + arrow-key to move around windows
+(setq column-number-mode t) ;; column numbers in modeline
 (setq-default indent-tabs-mode nil) ;; no tabs
-(setq inhibit-startup-message t) ;; no more startup message
+(setq inhibit-startup-message t)
 (setq mode-require-final-newline 1)
-(setq latex-run-command "pdflatex")
+
 ;; improves completion performance
 (setq gc-cons-threshold (* 100 1024 1024))
 (setq read-process-output-max (* 1024 1024))
@@ -40,13 +39,35 @@
 (global-set-key [f12] 'smerge-keep-upper)
 
 ;; extra hooks
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(add-hook 'before-save-hook
+          (lambda ()
+            (unless (derived-mode-p 'markdown-mode)
+              'delete-trailing-whitespace)))
 
-(use-package flycheck :ensure)
-(use-package helm :ensure :config (helm-mode))
+(add-hook 'latex-mode-hook
+          (lambda ()
+            (setq latex-run-command "pdflatex")))
+
+(use-package flycheck
+  :ensure
+  :hook prog-mode
+  :config
+  (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
+
+(use-package helm
+  :ensure
+  :bind
+  (("M-x" . helm-M-x)
+   ("C-x C-f" . helm-find-files))
+  :config
+  (helm-mode 1))
+
 (use-package helm-xref :ensure :after (helm))
 (use-package which-key :ensure :config (which-key-mode))
 (use-package zenburn-theme :ensure :config (load-theme 'zenburn t))
+(use-package org :ensure)
+(use-package magit :ensure)
+(use-package mood-line :ensure :config (mood-line-mode))
 
 (use-package company
   :ensure
@@ -68,9 +89,10 @@
 
 (use-package yasnippet
   :ensure
+  :hook (prog-mode . yas-minor-mode)
+  :commands yas-reload-all
   :config
-  (yas-reload-all)
-  (add-hook 'prog-mode-hook 'yas-minor-mode))
+  (yas-reload-all))
 
 (use-package go-to-char
   :bind (("C-f" . go-to-char-forward))
@@ -78,12 +100,15 @@
 
 (use-package rustic
   :ensure
+  :after (flycheck)
   :init
   ;; to use rustic-mode even if rust-mode also installed
   (setq auto-mode-alist (delete '("\\.rs\\'" . rust-mode) auto-mode-alist))
   ;; (setq rustic-format-trigger 'on-save)
   (setq rustic-rustfmt-args "--edition=2021")
-  (setq rustic-lsp-client 'eglot))
+  (setq rustic-lsp-client 'eglot)
+  :config
+  (push 'rustic-clippy flycheck-checkers))
 
 (use-package projectile
   :ensure
@@ -97,20 +122,17 @@
 
 (use-package visual-regexp-steroids
   :ensure
-  :bind
-  ("C-c q" . vr/query-replace)
-  ("C-r" . vr/isearch-backward)
-  ("C-s" . vr/isearch-forward))
+  :bind ("C-c q" . vr/query-replace))
 
-(use-package org :ensure)
-(use-package magit :ensure)
 (use-package deadgrep
-  :defer t
   :ensure
   :bind ("M-s r" . deadgrep))
+
 (use-package auto-package-update
-  :ensure
-  :config (setq auto-package-update-delete-old-versions t))
+  :config
+  (setq auto-package-update-delete-old-versions t)
+  (setq auto-package-update-hide-results t)
+  (auto-package-update-maybe))
 
 (use-package typescript-mode
   :ensure
