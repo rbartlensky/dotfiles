cb558f2433ef8e411d388033292c99faeeae659d
Robert Bartlensky <robert@electricwisp.com>
2024-09-17 01:00:31 +0100

    emacs: Remove unnecessary plugins.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -46,6 +46,15 @@
           (lambda ()
             (setq latex-run-command "pdflatex")))
 
+(setq major-mode-remap-alist
+ '((yaml-mode . yaml-ts-mode)
+   (bash-mode . bash-ts-mode)
+   (js2-mode . js-ts-mode)
+   (typescript-mode . typescript-ts-mode)
+   (json-mode . json-ts-mode)
+   (python-mode . python-ts-mode)
+   (rust-mode . rust-ts-mode)))
+
 (use-package helm
   :ensure
   :bind
@@ -54,15 +63,15 @@
   :hook ((after-init . helm-mode)))
 
 (use-package avy
-  :ensure t
+  :ensure
   :bind (("C-c j" . avy-goto-line)
          ("C-:"   . avy-goto-char-timer)))
 
 (use-package helm-xref :ensure :after (helm))
 (use-package which-key :ensure :config (which-key-mode))
-;; (use-package zenburn-theme :ensure :config (load-theme 'zenburn t))
 (use-package modus-themes :ensure :config (load-theme 'modus-operandi-tinted t))
 (use-package org :mode "\\.org$")
+(use-package rust-ts-mode :mode "\\.rs$")
 
 (use-package magit
   :ensure
@@ -71,24 +80,11 @@
   (remove-hook 'magit-status-sections-hook 'magit-insert-unpulled-from-pushremote)
   (remove-hook 'magit-status-sections-hook 'magit-insert-unpulled-from-upstream))
 
-;; (use-package mood-line :ensure :config (mood-line-mode))
-
 (use-package company
   :ensure
   :hook (after-init . global-company-mode)
   :bind (("M-<tab>" . company-complete-common-or-cycle))
   :custom (company-idle-delay nil))
-
-(defun rb/c-mode ()
-  (c-set-offset 'substatement-open 0)
-  (setq-default c-default-style "linux"))
-(add-hook 'c-mode-hook 'rb/c-mode)
-
-(defun rb/c++-mode ()
-  (c-set-offset 'substatement-open 0)
-  (setq-default c-basic-offset 4)
-  (setq-default c-indent-level 4))
-(add-hook 'c++-mode-hook 'rb/c++-mode)
 
 (use-package yasnippet
   :ensure
@@ -101,25 +97,6 @@
   :bind (("C-f" . go-to-char-forward))
   :load-path "~/.emacs.d/go-to-char")
 
-(use-package rustic
-  :ensure
-  :init
-  ;; to use rustic-mode even if rust-mode also installed
-  (setq auto-mode-alist (delete '("\\.rs\\'" . rust-mode) auto-mode-alist))
-  ;; (setq rustic-format-trigger 'on-save)
-  (setq rustic-rustfmt-args "--edition=2021")
-  (setq rustic-lsp-client 'eglot))
-
-(use-package projectile
-  :ensure
-  :bind
-  ("M-s s" . projectile-replace)
-  ("M-s j" . projectile-find-other-file)
-  ("M-s c" . projectile-compile-project)
-  ("M-s f" . projectile-find-file)
-  ("M-s t" . projectile-regenerate-tags)
-  ("M-s x" . projectile-run-project))
-
 (use-package visual-regexp-steroids
   :ensure
   :bind ("C-c q" . vr/query-replace))
@@ -129,28 +106,24 @@
   :bind ("M-s r" . deadgrep))
 
 (use-package auto-package-update
+  :ensure
   :config
   (setq auto-package-update-delete-old-versions t)
   (setq auto-package-update-hide-results t))
 
 (use-package typescript-mode
-  :ensure
-  :init (define-derived-mode typescript-tsx-mode typescript-mode "tsx")
+  :init (define-derived-mode typescript-tsx-mode typescript-ts-mode "tsx")
   :config
   (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . typescript-tsx-mode)))
-
-(use-package treesit-auto
-  :ensure)
 
 (use-package flycheck
   :ensure
   :init (global-flycheck-mode))
 
 (use-package eglot
-  :ensure
-  :pin gnu-devel
-  :hook (((c-mode c++-mode python-mode typescript-tsx-mode go-mode) . eglot-ensure)
-         (eglot-managed-mode . (lambda () (eldoc-mode -1))))
+  :hook (((c-mode c++-mode python-mode rust-ts-mode typescript-tsx-mode go-mode) . eglot-ensure)
+         (eglot-managed-mode . (lambda () (eldoc-mode -1)))
+         (eglot-managed-mode . (lambda () (flymake-mode -1))))
   :bind (:map eglot-mode-map
               ("C-c a" . eglot-code-actions)
               ("C-c r" . eglot-rename)
@@ -162,16 +135,8 @@
   (put 'eglot-warning 'flymake-overlay-control nil)
   (put 'eglot-error 'flymake-overlay-control nil)
   (setq eglot-ignored-server-capabilities '(:inlayHintProvider :documentOnTypeFormattingProvider))
-  (add-hook 'eglot--managed-mode-hook (lambda () (flymake-mode -1)))
   (add-to-list 'eglot-server-programs
                '(typescript-tsx-mode . ("typescript-language-server" "--stdio"))))
-
-(use-package esup
-  :ensure
-  ;; To use MELPA Stable use ":pin melpa-stable",
-  :pin melpa
-  :config
-  (setq esup-depth 0))
 
 (use-package flycheck-eglot
   :ensure
@@ -179,6 +144,4 @@
   :config
   (global-flycheck-eglot-mode 1))
 
-;; if we have specific configs on another machine, load them up
-(if (file-exists-p "~/.emacs.d/extras.el")
-    (load "~/.emacs.d/extras.el"))
+;;; init.el ends here
